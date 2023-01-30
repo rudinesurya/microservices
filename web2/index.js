89b6cb74675430e0461639df64bdad28350f1cf5
Rudine <rudinesurya@gmail.com>
2023-01-30 15:06:14 +0000

    first commit

---
+++ web2/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotEnv = require("dotenv");
+
+const app = express();
+dotEnv.config();
+
+app.get('/', (req, res) => {
+  res.send('/');
+});
+
+app.get('/ping', (req, res) => {
+  res.send('Successful response from web2.');
+});
+
+app.listen(process.env.PORT, () => console.log(`Example app is listening on port ${process.env.PORT}.`));
